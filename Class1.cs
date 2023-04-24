d61a75cda861be3e545246875ea7fae6b41e2934
Armando Rodríguez <jarodriguezvera@gmail.com>
2023-04-24 12:09:55 -0600

    Agregar archivos de proyecto.

---
+++ Class1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+
+namespace Usuarios_G2_2023_II
+{
+    public class Persona
+    {
+        private string usuario;
+        private string password;
+
+        public string Usuario {
+            get => usuario;
+            set {
+                if( value == "")
+                {
+                    usuario = "invitado";
+                }
+                else
+                {
+                    usuario = value;
+                }               
+                          
+            } 
+        }
+        public string Password { 
+            get => password;
+            set
+            {
+                if( value == "")
+                {
+                    password = "12345";
+                }
+                else
+                {
+                    password = value;
+                }                
+            }
+        }
+
+    }
+
+    public class GeneraUsuario
+    {
+        public ArrayList usuarios;
+
+        public GeneraUsuario()
+        {
+            usuarios = new ArrayList();
+        }
+
+        public void Generar()
+        {
+           // usuarios.Add(new   );
+        }
+
+    }
+}
